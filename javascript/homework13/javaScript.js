01e571d8f21df515ee7cc8eb88b8904296567804
unknown <ps2014yeganian@gmail.com>
2019-08-23 20:49:58 +0300

    js 13

--- javascript/homework13/javaScript.js
+++ javascript/homework13/javaScript.js
@@ -1,17 +1,32 @@
-const ChangeThemeBtn = document.querySelector('#change-theme-btn');
 
-ChangeThemeBtn.addEventListener('click', ()=> {
+
+function changeTheme() {
+
+    ChangeThemeBtn.classList.toggle('dark');
     document.querySelector('body').classList.toggle('dark-theme');
     document.querySelector('.menubar').classList.toggle('dark-theme');
     document.querySelector('.image').classList.toggle('dark-theme');
     document.querySelector('.text-container').classList.toggle('dark-theme');
     document.querySelector('.footer').classList.toggle('footer-dark-theme');
+    document.querySelector('.top-navbar').classList.toggle('footer-dark-theme');
+
+
+    if(ChangeThemeBtn.classList.contains('dark')){
+        localStorage.setItem('PageTheme', 'DarkTheme');
+    }else{
+        localStorage.setItem('PageTheme', 'WhiteTheme');
+    }
+
+};
+
+const ChangeThemeBtn = document.querySelector('#change-theme-btn');
+ChangeThemeBtn.addEventListener('click', changeTheme );
+
+if (localStorage.getItem('PageTheme') === 'DarkTheme') {
+    changeTheme()
+};
 
 
 
 
-    document.querySelector('.main-name').classList.toggle('.main-name-dark-theme');
-    document.querySelector('.main-name').classList.toggle('.main-name-dark-theme');
-.menubar-item:hover {
-        color: black;
-})
+
